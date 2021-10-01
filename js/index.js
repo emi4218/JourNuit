8c37d75c63e39c5da2ac7085995024030b5bb71e
emi4218 <emi4218@gmail.com>
2021-10-01 14:58:46 +0200

    moche mais fonctionnel

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 window.addEventListener('load', () => {
 
+    let ld = document.querySelector('.lightDark');
+    let body = document.body;
+    let rond = document.querySelector('.rondBouton');
+
+    ld.addEventListener('click', event => {
+        if (body.classList.contains('light')) {
+            body.classList.remove('light');
+            body.classList.add('dark');
+            rond.style.marginLeft = '25px';
+        } else {
+            body.classList.remove('dark');
+            body.classList.add('light');
+            rond.style.marginLeft = '1px'
+        }
+    })
 
 
 });
